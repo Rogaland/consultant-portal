5461a398f4776199fd1542275f4144d0503ca23c
Mads Tordal <mads.tordal@gmail.com>
2017-03-24 13:16:23 +0100

    Set inviter full name in consultantToken.

--- consultant-portal-backend/src/main/java/no/rogfk/consultant/utilities/InviteUrlGenerator.java
+++ consultant-portal-backend/src/main/java/no/rogfk/consultant/utilities/InviteUrlGenerator.java
@@ -2,7 +2,9 @@ package no.rogfk.consultant.utilities;
 
 import no.rogfk.consultant.model.Consultant;
 import no.rogfk.consultant.model.ConsultantToken;
+import no.rogfk.consultant.model.HostEmployee;
 import no.rogfk.consultant.service.ConfigService;
+import no.rogfk.consultant.service.HostEmployeeService;
 import no.rogfk.jwt.SpringJwtTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,12 +16,17 @@ public class InviteUrlGenerator {
     private ConfigService configService;
 
     @Autowired
+    private HostEmployeeService hostEmployeeService;
+
+    @Autowired
     private SpringJwtTokenizer springJwtTokenizer;
 
     public String get(Consultant consultant) {
         ConsultantToken consultantToken = new ConsultantToken();
         consultantToken.setId(consultant.getCn());
-        consultantToken.setInviterName("Ole Olsen");
+
+        HostEmployee hostEmployee = hostEmployeeService.getHostEmployee(consultant.getOwner());
+        consultantToken.setInviterName(hostEmployee.getFullname());
 
         return springJwtTokenizer.createWithUrl(configService.getBaseUrl(), "t", consultantToken);
     }
